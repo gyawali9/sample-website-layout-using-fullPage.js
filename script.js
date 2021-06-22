c2b6042275aaf61a788a93ed404e76c4baae64be
gyawali9 <roshangyawali9@gmail.com>
2021-06-22 20:57:48 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  navigation: true,
+  navigationTooltips: ["Home", "About", "Skills", "Contact"],
+  showActiveTooltip: true,
+  controlArrows: false,
+
+  keyboardScrolling: true,
+
+  slidesNavigation: true,
+  slidesNavPosition: "top",
+});
